3b080a36c567ce63a2ecae28c911ee7be32a7ab1
1024 <rk.hh@live.com>
2016-11-28 19:38:11 +0800

    Rename variables

--- bars.py
+++ bars.py
@@ -7,24 +7,24 @@ import geopy.distance
 
 def load_data(filepath):
     with open(filepath, encoding="utf-8") as json_file:
-        data = json.loads(json_file.read())
-    return data
+        bars = json.loads(json_file.read())
+    return bars
 
 
-def get_biggest_bar(data):
-    bar = max(data, key=lambda x: x["Cells"]["SeatsCount"])
+def get_biggest_bar(bars):
+    bar = max(bars, key=lambda x: x["Cells"]["SeatsCount"])
     return bar["Cells"]["Name"]
 
 
-def get_smallest_bar(data):
-    bar = min(data, key=lambda x: x["Cells"]["SeatsCount"])
+def get_smallest_bar(bars):
+    bar = min(bars, key=lambda x: x["Cells"]["SeatsCount"])
     return bar["Cells"]["Name"]
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_closest_bar(bars, longitude, latitude):
     coordinates_list = [(bar["Cells"]["Name"],
-            geopy.Point(bar["Cells"]["geoData"]["coordinates"][0],
-                bar["Cells"]["geoData"]["coordinates"][1])) for bar in data]
+            geopy.Point(bar["Cells"]["geobars"]["coordinates"][0],
+                bar["Cells"]["geobars"]["coordinates"][1])) for bar in bars]
     coordinate = geopy.Point(longitude, latitude)
     distances = [(bar[0], geopy.distance.distance(bar[1], 
             coordinate).km) for bar in coordinates_list]
@@ -36,13 +36,13 @@ if __name__ == '__main__':
     parser.add_argument("json_file")
     namespace = parser.parse_args()
     try:
-        data = load_data(namespace.json_file)
+        bars = load_data(namespace.json_file)
     except AttributeError:
         print("Пожалуйста, запустите программу, передав как аргумент json-файл со списком баров")
         sys.exit()
-    print("Самый большой бар - {}".format(get_biggest_bar(data)))
-    print("Самый маленький бар - {}".format(get_smallest_bar(data)))
+    print("Самый большой бар - {}".format(get_biggest_bar(bars)))
+    print("Самый маленький бар - {}".format(get_smallest_bar(bars)))
     longitude = input("Input your longitude:")
     latitude = input("Input your latitude:")
     print("Ближайший к указанным координатам бар - \
-{}".format(get_closest_bar(data, longitude, latitude)))
+{}".format(get_closest_bar(bars, longitude, latitude)))
